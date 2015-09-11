a0461105e36472aa5d1abf4b021f339547bce6c9
Tanner May <tannerjmay@gmail.com>
2015-09-11 09:58:23 -0600

    without_doubles complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,24 @@
-module MethodsQuiz
-	
-	
+module MethodsQuiz2
+
+	def without_doubles(d1, d2, no_doubles)
+		if !no_doubles
+			sum = d1 + d2
+		elsif no_doubles
+			if d1 == d2
+				sum = dice_add_1(d1) + d2
+			end
+		end
+		return sum
+	end
 
 end
+
+private
+	def dice_add_1(n1)
+		if n1 == 6
+			n1 = 1
+		else
+			n1 += 1
+		end
+		return n1
+	end
